eedf0ee0257e24506d5d09f2f8358be7ae58b588
Alexander Pozas <a.posas@outlook.com>
2023-12-07 21:17:47 -0600

    Adding fonts and normalize

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,14 @@
-import '@/styles/globals.css'
+import { Outfit } from 'next/font/google';
+import '@/styles/normalize.css';
+import '@/styles/globals.css';
+
+
+// If loading a variable font, you don't need to specify the font weight
+const outfit = Outfit({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--ff-global'
+});
 
 export const metadata = {
   title: 'GuitarLa - Next.js',
@@ -7,7 +17,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${outfit.variable}`}
+    >
       <body>{children}</body>
     </html>
   )
